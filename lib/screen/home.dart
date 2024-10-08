7492165644f69b7a0ee52b0617db5c0b6154e101
Seamoon Pandey <hackedasocial@gmail.com>
2024-10-08 16:14:54 +0545

    clean up and set up

---
+++ lib/screen/home.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            debugPrint("Add address button pressed");
+          },
+          child: const Text("Add address"),
+        ),
+      ),
+    );
+  }
+}
